b409259eaaa1d8a0052a0a71e54a8520d5696086
eladlevy <elad@workey.co>
2016-04-07 18:26:18 +0300

    Action creator breaking changes - params passed as object, toString for an action. Added entity actions

--- src/actionCreator.js
+++ src/actionCreator.js
@@ -1,63 +1,63 @@
 import { collectionsProps } from './collection-tools';
 import { normalize, Schema, arrayOf} from 'normalizr';
+import uniqueId from 'lodash/utility/uniqueId';
 
 export const actionPrefix = 'redux-collection';
 
+function constBuilder(actionType, collectionName) {
+    const uppercasedCollectionName = collectionName.toUpperCase();
+    const uppercasedActionType = actionType.toUpperCase();
+    return`${actionPrefix}/${uppercasedActionType}_${uppercasedCollectionName}`
+}
+
 export function actionCreator() {
-    const actionWrapper = function(actionType) {
-        return function (collectionName, data) {
-            const uppercasedCollectionName = collectionName.toUpperCase();
-            const uppercasedActionType = actionType.toUpperCase();
-            const { schema = new Schema(collectionName) } = collectionsProps[collectionName];
-            let id;
-            if (data) {
-                id = data.id;
-                var normalizedData = normalize(data, schema);
+    const actionWrapper = function(actionType, entityAction) {
+        return function ({collectionName, data, getString}) {
+            if (typeof collectionName !== 'string') {
+                throw new Error('Expected collectionName to be a string.');
             }
 
-            return {
-                type: `${actionPrefix}/${uppercasedActionType}_${uppercasedCollectionName}`,
-                id,
-                payload: normalizedData
-            };
-        }
-    };
-
-    const networkActionWrapper = function(actionType) {
-        return function (collectionName, data) {
-            const uppercasedCollectionName = collectionName.toUpperCase();
-            const uppercasedActionType = actionType.toUpperCase();
             const { schema = new Schema(collectionName) } = collectionsProps[collectionName];
+            const actionString = constBuilder(actionType, entityAction ? schema.getKey() : collectionName);
+
+            if (getString) return actionString;
+
             let id;
+            let payload;
             if (data) {
                 if (data instanceof Array) {
-                    var normalizedData = normalize(data, arrayOf(schema));
+                    payload = normalize(data, arrayOf(schema));
                 } else {
+                    if (!data.id) {
+                        data.id = uniqueId('client_');
+                    }
                     id = data.id;
-                    var normalizedData = normalize(data, schema);
+                    payload = normalize(data, schema);
                 }
             }
 
             return {
-                type: `${actionPrefix}/${uppercasedActionType}_${uppercasedCollectionName}`,
-                id,
-                payload: normalizedData
+                type: actionString,
+                meta: {id},
+                payload
             };
         }
     };
 
     return {
         addEntity: actionWrapper('add'),
-
         removeEntity: actionWrapper('remove'),
-
         updateEntity: actionWrapper('update'),
-
         resetEntity: actionWrapper('reset'),
 
-        //network events
-        requestCollection: networkActionWrapper('reset'),
-        requestCollectionSuccess: networkActionWrapper('reset'),
-        requestCollectionFailure: networkActionWrapper('reset')
+        //collection network events
+        requestCollection: actionWrapper('request'),
+        requestCollectionSuccess: actionWrapper('request_success'),
+        requestCollectionFailure: actionWrapper('request_failure'),
+
+        //entity network events
+        requestEntity: actionWrapper('request_entity', true),
+        requestEntitySuccess: actionWrapper('request_entity_success', true),
+        requestEntityFailure: actionWrapper('request_entity_success', true)
     };
 }
